721e4c10ba3295028b549f552d20654686f8bd0d
arush-shri <arush.sdl.shrivastava@gmail.com>
2025-02-22 10:05:55 +0530

    db connection added

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const application = express();
+// const contributionRoute = require('./routes/contributeRoute')
+const database = require('./controllers/dbConnector') 
+
+const cors = require("cors");
+
+application.use(cors());
+application.use(express.json());
+// application.use("/contribute", contributionRoute);
+
+
+application.get('/', (req,res) => {
+    res.status(200).send("Hello! Welcome to code reviewer");
+});
+
+database.initDb(
+    function(err){
+        application.listen(4000, () => {
+            if(err){
+                console.log(err)
+                throw err;
+            }
+            application.locals.database = database.getDb();
+            console.log("Server started at port 4000");
+        });
+    }
+);
