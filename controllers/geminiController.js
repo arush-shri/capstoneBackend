fb4bb327829ab67d69b79e41ec37e491137f4ce4
arush-shri <arush.sdl.shrivastava@gmail.com>
2025-02-22 11:39:32 +0530

    gemini response saved in db

--- controllers/geminiController.js
+++ controllers/geminiController.js
@@ -1,4 +1,5 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const crypto = require('crypto');
 
 async function GenerateResponse(database, body) {
     const genAI = new GoogleGenerativeAI(process.env.GEMINIAPI);
@@ -7,11 +8,13 @@ async function GenerateResponse(database, body) {
     const collection = database.collection("Reviews");
 
     const result = await model.generateContent(body.userPrompt);
-    const aiResponse = result.response.text();
+    const aiResponse = result.response.text().trim();
+    const uuid = crypto.randomBytes(16).toString('hex');
 
     const dataToStore = {
+        _id: uuid,
         user: body.userid,
-        dateTime: new Date.toUTCString(),
+        dateTime: new Date().toUTCString(),
         userPrompt: body.userPrompt,
         aiResponse: aiResponse
     };
