f8e317b38d5b6173dca4db783dda9703ab476b9a
arush-shri <arush.sdl.shrivastava@gmail.com>
2025-02-22 11:32:36 +0530

    gemini added

--- controllers/geminiController.js
+++ controllers/geminiController.js
@@ -1,11 +1,22 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-async function GenerateResponse(database, userPrompt) {
+async function GenerateResponse(database, body) {
     const genAI = new GoogleGenerativeAI(process.env.GEMINIAPI);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-    const result = await model.generateContent(userPrompt);
-    console.log(result.response.text());
-    return result.response.text();
+    const collection = database.collection("Reviews");
+
+    const result = await model.generateContent(body.userPrompt);
+    const aiResponse = result.response.text();
+
+    const dataToStore = {
+        user: body.userid,
+        dateTime: new Date.toUTCString(),
+        userPrompt: body.userPrompt,
+        aiResponse: aiResponse
+    };
+
+    await collection.insertOne(dataToStore);
+    return aiResponse;
 }
 module.exports = { GenerateResponse };
