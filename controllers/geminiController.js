0afdcfe39973133ea7cb04b6135f82f47d107d98
arush-shri <arush.sdl.shrivastava@gmail.com>
2025-02-22 12:04:19 +0530

    ai token validation added

--- controllers/geminiController.js
+++ controllers/geminiController.js
@@ -2,24 +2,36 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const crypto = require('crypto');
 
 async function GenerateResponse(database, body) {
-    const genAI = new GoogleGenerativeAI(process.env.GEMINIAPI);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const userCollection = database.collection("Users");
+    const userData = await userCollection.findOneAndUpdate(
+        { _id: body.userid, tokens: { $gt: 0 } },
+        { $inc: { tokens: -1 } },
+        { returnDocument: 'after' }
+    );
 
-    const collection = database.collection("Reviews");
+    if(userData){
+        const collection = database.collection("Reviews");
+        const genAI = new GoogleGenerativeAI(process.env.GEMINIAPI);
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-    const result = await model.generateContent(body.userPrompt);
-    const aiResponse = result.response.text().trim();
-    const uuid = crypto.randomBytes(16).toString('hex');
 
-    const dataToStore = {
-        _id: uuid,
-        user: body.userid,
-        dateTime: new Date().toUTCString(),
-        userPrompt: body.userPrompt,
-        aiResponse: aiResponse
-    };
+        const result = await model.generateContent(body.userPrompt);
+        const aiResponse = result.response.text().trim();
+        const uuid = crypto.randomBytes(16).toString('hex');
 
-    await collection.insertOne(dataToStore);
-    return aiResponse;
+        const dataToStore = {
+            _id: uuid,
+            user: body.userid,
+            dateTime: new Date().toUTCString(),
+            userPrompt: body.userPrompt,
+            aiResponse: aiResponse
+        };
+
+        await collection.insertOne(dataToStore);
+
+        return aiResponse;
+    } else{
+        return "Out of tokens"
+    }
 }
 module.exports = { GenerateResponse };
